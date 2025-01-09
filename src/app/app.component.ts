0d990b92166e1019e6ca164b09ea465338c64f8c
Albert Emanuel <111556726+AlbertEmanuel@users.noreply.github.com>
2025-01-09 16:19:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reative-form';
+  // reactiveForm: FormGroup = new FormGroup({});
+  reactiveForm!: FormGroup; 
+  
+  ngOnInit(){
+    this.reactiveForm = new FormGroup({
+      firstName: new FormControl(null),
+      lastName: new FormControl(null),
+      email: new FormControl(null),
+      userName: new FormControl(null),
+      dob: new FormControl(null),
+      gender: new FormControl('male'),
+      street: new FormControl(null),
+      city: new FormControl(null),
+      country: new FormControl(null),
+      region: new FormControl(null),
+      postCode: new FormControl(null),
+    })
+  }
+
+  onSubmit(){
+    console.log('Reactive Form', this.reactiveForm.value.firstName);
+   
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ReactiveLoginComponent } from './reactive-login/reactive-login.component';
+import { PractiveFormComponent } from './practive-form/practive-form.component';
+import { SingupComponent } from './auth/singup/singup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    ReactiveLoginComponent,
+    PractiveFormComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  ReactiveFormsModule,
+  FormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
